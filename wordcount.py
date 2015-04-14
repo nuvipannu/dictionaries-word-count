2f6a7ed624e37554d33fa4d4fb784519705a09d6
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 20:12:20 -0700

    updates

--- wordcount.py
+++ wordcount.py
@@ -2,17 +2,18 @@ test_name = open('test.txt')
 
 
 # def count_num_words(test_name):
-#     word_count = {} #open dictionary
+#word_count = {} #open dictionary
 
 for line in test_name:
     line = line.strip()
     new_line = line.rstrip()
     space_out = new_line.replace(" ", "\n") 
-    
    
     """needed to strip blank lines in between each line in addition to 
     rstrip. Didn't know until ran for loop and printed the entire text doc"""
         
+   
+
     print space_out
  #    for w in words:
  #         if word_count.get(w) == None:
@@ -20,6 +21,6 @@ for line in test_name:
  #         else:
  #             word_count[w] += 1
 
- #     print word_count
+ #          print word_count
 
  # count_num_words('test.txt')
